42dcf9ea9703ea1cf3bc2f60638a12b3ff1f7b8b
Nathan Sodja <sodjanathan@gmail.com>
2024-03-22 19:50:16 +0000

    text to speech

--- text_to_speech/index.js
+++ text_to_speech/index.js
@@ -1,13 +1,23 @@
-import 'dotenv/config'
-import { HfInference } from '@huggingface/inference'
+import "dotenv/config";
+import { HfInference } from "@huggingface/inference";
 
 // Create your Hugging Face Token: https://huggingface.co/settings/tokens
 // Set your Hugging Face Token: https://scrimba.com/dashboard#env
 // Learn more: https://scrimba.com/links/env-variables
-const hf = new HfInference(process.env.HF_TOKEN)
+const hf = new HfInference(process.env.HF_TOKEN);
 
-console.log(process.env.HF_TOKEN);
+console.log(process.env);
 // Hugging Face Inference API docs: https://huggingface.co/docs/huggingface.js/inference/README
 
-const text = "It's an exciting time to be an A.I. engineer."
+const text = "It's an exciting time to be an A.I. engineer.";
 
+const response = await hf.textToSpeech({
+  inputs: text,
+  model: "espnet/kan-bayashi_ljspeech_vits",
+});
+
+console.log(response);
+
+const audioElement = document.getElementById('speech')
+const speechUrl = URL.createObjectURL(response)
+audioElement.src = speechUrl
